166defa17216f1ea30deff54229268f92a1677b2
Shubhendra Vyas <shubhendravyas51@gmail.com>
2018-10-26 11:38:18 +0530

    correction in application-json to application/json

--- webhook.py
+++ webhook.py
@@ -19,7 +19,7 @@ def webhook():
     res = json.dumps(req, indent=4)
     
     r = make_response(res)
-    r.headers['Content-Type'] = 'application-json'
+    r.headers['Content-Type'] = 'application/json'
     return r
 
 def makeResponse(req):

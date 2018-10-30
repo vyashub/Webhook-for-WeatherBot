5a6bfb5a04fd6fd4976249d98c356cd91a3f5e8b
Shubhendra Vyas <shubhendravyas51@gmail.com>
2018-10-30 13:04:24 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -27,6 +27,8 @@ def makeResponse(req):
     parameters = result.get("parameters")
     city = parameters.get("geo-city")
     date = parameters.get("date")
+    city = 'New York'
+    date = '2018-10-31'
     r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=06f070197b1f60e55231f8c46658d077')
     json_object = r.json()
     weather=json_object['list']
@@ -37,27 +39,11 @@ def makeResponse(req):
     speech = "The forecast for"+city+"for "+date+" is "+condition
     return {
     "speech": speech,
-    "displayText": speech,
-    "source": "apiai-weather-webhook"
+    "displayText": speech
+#    "source": "apiai-weather-webhook"
     }
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
